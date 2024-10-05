b50175b4ff4ab9e293b63563ccd29762ec823538
Ignacio de los Rios <ignacio.delosrios1@yahoo.com>
2024-10-05 09:17:53 +0200

    feat(ISSUE-2) add pki secrets engine

--- demo/terraform/main.tf
+++ demo/terraform/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "kreuzwerker/docker"
       version = "~> 2.0"
     }
+    vault = {
+      source  = "hashicorp/vault"
+      version = "~> 4.0"
+    }
   }
 }
 
@@ -30,5 +34,29 @@ resource "docker_container" "vault" {
     "VAULT_DEV_LISTEN_ADDRESS=0.0.0.0:8200",
     "VAULT_ADDR=http://0.0.0.0:8200"
   ]
+}
 
+# Wait for Vault server to be ready
+resource "null_resource" "wait_for_vault" {
+  depends_on = [docker_container.vault]
+
+  provisioner "local-exec" {
+    command = "while ! curl -s http://0.0.0.0:8200/v1/sys/health >/dev/null; do echo 'Waiting for Vault...'; sleep 1; done"
+  }
+}
+
+# Add the Vault provider
+provider "vault" {
+  address = "http://0.0.0.0:8200"
+  token   = "myroot"
+}
+
+resource "vault_mount" "pki" {
+  path                      = "pki"
+  type                      = "pki"
+  default_lease_ttl_seconds = 3600
+  max_lease_ttl_seconds     = 86400
+  description               = "PKI secrets engine"
+
+  depends_on = [null_resource.wait_for_vault]
 }

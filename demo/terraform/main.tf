625a09fe30cdde84ad9e6dddba514f05692d2f09
Ignacio de los Rios <ignacio.delosrios1@yahoo.com>
2024-10-05 23:13:22 +0200

    feat(ISSUE-2) enable vault pki with ca

--- demo/terraform/main.tf
+++ demo/terraform/main.tf
@@ -8,15 +8,28 @@ terraform {
       source  = "hashicorp/vault"
       version = "~> 4.0"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
   }
+}
+
+variable "vault_address" {
+  default = "http://127.0.0.1:8200"
 }
 
 provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
 
+provider "vault" {
+  address = var.vault_address
+  token   = "myroot"
+}
+
 resource "docker_image" "vault" {
-  name         = "vault:1.13.3"
+  name         = "hashicorp/vault:1.17.5"
   keep_locally = false
 }
 
@@ -36,19 +49,12 @@ resource "docker_container" "vault" {
   ]
 }
 
-# Wait for Vault server to be ready
 resource "null_resource" "wait_for_vault" {
   depends_on = [docker_container.vault]
 
   provisioner "local-exec" {
-    command = "while ! curl -s http://0.0.0.0:8200/v1/sys/health >/dev/null; do echo 'Waiting for Vault...'; sleep 1; done"
+    command = "while ! curl -s ${var.vault_address}/v1/sys/health >/dev/null; do echo 'Waiting for Vault...'; sleep 1; done"
   }
-}
-
-# Add the Vault provider
-provider "vault" {
-  address = "http://0.0.0.0:8200"
-  token   = "myroot"
 }
 
 resource "vault_mount" "pki" {
@@ -59,4 +65,30 @@ resource "vault_mount" "pki" {
   description               = "PKI secrets engine"
 
   depends_on = [null_resource.wait_for_vault]
+}
+
+resource "vault_pki_secret_backend_root_cert" "root_cert" {
+  backend = vault_mount.pki.path
+  type    = "internal"
+
+  common_name = "svc"
+  ttl         = "87600h"
+
+  depends_on = [vault_mount.pki]
+}
+
+resource "local_file" "ca_cert" {
+  content  = vault_pki_secret_backend_root_cert.root_cert.certificate
+  filename = "${path.module}/CA_cert.crt"
+
+  depends_on = [vault_pki_secret_backend_root_cert.root_cert]
+}
+
+resource "vault_pki_secret_backend_config_urls" "pki_urls" {
+  depends_on = [vault_pki_secret_backend_root_cert.root_cert]
+
+  backend = vault_mount.pki.path
+
+  issuing_certificates    = ["${var.vault_address}/v1/${vault_mount.pki.path}/ca"]
+  crl_distribution_points = ["${var.vault_address}/v1/${vault_mount.pki.path}/crl"]
 }
